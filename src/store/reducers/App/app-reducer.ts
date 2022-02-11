62ed07cc0d9bb745cf6ab703b92dc0733b7127cf
Andrei <yantsevich92@gmail.com>
2022-02-12 00:35:05 +0300

    connected eslint

---
+++ src/store/reducers/App/app-reducer.ts
@@ -0,0 +1,11 @@
+type AppState = {};
+type AppActions = any;
+
+const initialState: AppState = {};
+
+export const appReducer = (state = initialState, action: AppActions): AppState => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
---
+++ src/store/reducers/Auth/auth-reducer.ts
@@ -0,0 +1,11 @@
+type AuthState = {};
+type AuthActions = any;
+
+const initialState: AuthState = {};
+
+export const authReducer = (state = initialState, action: AuthActions): AuthState => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
